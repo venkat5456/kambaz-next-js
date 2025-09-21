918531002b92657c901a2bf3d7a229240397abd2
Venkat Atluri <venkyatluri@mac.mynetworksettings.com>
2025-09-21 19:55:30 -0400

    all changes done

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,19 @@
 import Link from "next/link";
 
-export default function Landing() {
+export default function Home() {
   return (
-    <main style={{ padding: "1rem" }}>
-      <h1>Landing Page</h1>
-      <p><strong>Student:</strong> Atluri Sai Venkat</p>
-      <p><strong>Section:</strong> CS5610.02</p>
-
-      <h2>Navigation</h2>
+    <div id="wd-landing-page">
+      <h2>Landing Page</h2>
+      <p>Student: Atluri Sai Venkat</p>
+      <p>Section: CS5610.02</p>
       <ul>
-        <li><Link href="/Lab1">Lab 1 Page</Link></li>
-        <li><Link href="/Dashboard">Kambaz Application</Link></li>
+        <li><Link href="/Labs/Lab1">Lab 1 Page</Link></li>
+        <li><Link href="/Labs">All Labs</Link></li>
+        <li><Link href="/Account/Signin">Kambaz Application</Link></li>
+        <li><a href="https://github.com/venkat5456/kambaz-next-js" target="_blank">Source Code</a></li>
+        <li><a href="https://github.com/venkat5456/kambaz-next-js" target="_blank">Repository (Kambaz + Labs)</a></li>
       </ul>
-
-      <h2>Source Code</h2>
-      <ul>
-        <li>
-          <a href="https://github.com/venkat5456/kambaz-next-js" target="_blank">
-            Repository (Kambaz + Labs)
-          </a>
-        </li>
-      </ul>
-    </main>
+    </div>
   );
 }
 
