ec51296cd40b28f23f3b093836921dc09a99d483
Venkat Atluri <venkyatluri@mac.mynetworksettings.com>
2025-09-22 15:26:25 -0400

    final one

--- app/(Kambaz)/Account/Signin/page.tsx
+++ app/(Kambaz)/Account/Signin/page.tsx
@@ -5,11 +5,13 @@ export default function Signin() {
   return (
     <div id="wd-signin-screen">
       <h3>Sign in</h3>
-      <input placeholder="username" className="wd-username" /><br/>
-      <input placeholder="password" type="password" className="wd-password" /><br/>
-      <Link href="/Dashboard" id="wd-signin-btn">Sign in</Link><br/>
-      <Link href="Signup" id="wd-signup-link">Sign up</Link>
+      <input placeholder="username" /><br />
+      <input placeholder="password" type="password" /><br />
+      <Link href="/Dashboard">
+        <button>Sign in</button>
+      </Link>
     </div>
   );
 }
+
 
--- app/(Kambaz)/Courses/[cid]/page.tsx
+++ app/(Kambaz)/Courses/[cid]/page.tsx
@@ -1,6 +1,17 @@
-import { redirect } from "next/navigation";
+import Link from "next/link";
 
-export default async function CoursesPage({ params }: { params: Promise<{ cid: string }> }) {
-  const { cid } = await params;
-  redirect(`/Courses/${cid}/Home`);
+export default function Course({ params }: { params: { cid: string } }) {
+  return (
+    <div id="wd-course">
+      <h2>Course ID: {params.cid}</h2>
+      <ul>
+        <li><Link href={`/Courses/${params.cid}/Modules`}>Modules</Link></li>
+        <li><Link href={`/Courses/${params.cid}/Assignments`}>Assignments</Link></li>
+        <li><Link href={`/Courses/${params.cid}/Quizzes`}>Quizzes</Link></li>
+        <li><Link href={`/Courses/${params.cid}/Grades`}>Grades</Link></li>
+        <li><Link href={`/Courses/${params.cid}/People`}>People</Link></li>
+      </ul>
+    </div>
+  );
 }
+
