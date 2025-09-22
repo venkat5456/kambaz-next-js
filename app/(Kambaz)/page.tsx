602ba6ab591d26cf01984310b655bec0bace7f70
Venkat Atluri <venkyatluri@Venkats-MacBook-Pro.local>
2025-09-22 18:03:27 -0400

    fixed everything

--- app/(Kambaz)/page.tsx
+++ app/(Kambaz)/page.tsx
@@ -1,6 +1,18 @@
-import { redirect } from "next/navigation";
+import Link from "next/link";
 
-export default function Kambaz() {
-  redirect("/Account/Signin");
-  return <div>Kambaz</div>;
+export default function Home() {
+  return (
+    <div id="wd-landing-page">
+      <h2>Landing Page</h2>
+      <p>Student: Atluri Sai Venkat</p>
+      <p>Section: CS5610.02</p>
+      <ul>
+        <li><Link href="/Labs/Lab1">Lab 1 Page</Link></li>
+        <li><Link href="/Labs">All Labs</Link></li>
+        <li><Link href="/Account/Signin">Kambaz Application</Link></li>
+        <li><a href="https://github.com/venkat5456/kambaz-next-js" target="_blank">Source Code</a></li>
+        <li><a href="https://github.com/venkat5456/kambaz-next-js" target="_blank">Repository (Kambaz + Labs)</a></li>
+      </ul>
+    </div>
+  );
 }
