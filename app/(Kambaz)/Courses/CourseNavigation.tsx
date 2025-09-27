ffb0b7aa1525cf45d7239406c576e6c67ada99a8
Venkat Atluri <venkyatluri@mac.mynetworksettings.com>
2025-09-27 13:24:41 -0400

    assignment 1;

--- app/(Kambaz)/Courses/CourseNavigation.tsx
+++ app/(Kambaz)/Courses/CourseNavigation.tsx
@@ -1,14 +1,27 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export default function CourseNavigation() {
+  const pathname = usePathname();
+  // extract cid from the URL, e.g. /Courses/1234/Home
+  const parts = pathname.split("/");
+  const cid = parts[2]; // "1234"
+
   return (
     <div id="wd-course-navigation">
-      <a href="Home">Home</a>
-      <a href="Modules">Modules</a>
-      <a href="Piazza">Piazza</a>
-      <a href="Zoom">Zoom</a>
-      <a href="Assignments">Assignments</a>
-      <a href="Quizzes">Quizzes</a>
-      <a href="Grades">Grades</a>
-      <a href="People">People</a>
+      <ul>
+        <li><Link href={`/Courses/${cid}/Home`}>Home</Link></li>
+        <li><Link href={`/Courses/${cid}/Modules`}>Modules</Link></li>
+        <li><Link href={`/Courses/${cid}/Piazza`}>Piazza</Link></li>
+        <li><Link href={`/Courses/${cid}/Zoom`}>Zoom</Link></li>
+        <li><Link href={`/Courses/${cid}/Assignments`}>Assignments</Link></li>
+        <li><Link href={`/Courses/${cid}/Quizzes`}>Quizzes</Link></li>
+        <li><Link href={`/Courses/${cid}/Grades`}>Grades</Link></li>
+        <li><Link href={`/Courses/${cid}/People`}>People</Link></li>
+      </ul>
     </div>
   );
 }
+
