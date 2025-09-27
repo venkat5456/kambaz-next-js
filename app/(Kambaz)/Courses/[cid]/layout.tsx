c60f23c5d2393caa964883b68a8c7cd7820ca1ad
Venkat Atluri <venkyatluri@mac.mynetworksettings.com>
2025-09-27 11:17:40 -0400

    assignment1

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -25,3 +25,4 @@ export default function CoursesLayout({ children }: { children: ReactNode }) {
 
 
 
+
--- app/Labs/Lab3/page.tsx
+++ app/Labs/Lab3/page.tsx
@@ -1,14 +1,2 @@
-export default function Lab3() {
-  return (
-    <div id="wd-lab3">
-      <h2>Lab 3: Images</h2>
 
-      <h3>Starship</h3>
-      <img src="/images/starship.jpg" alt="Starship" width={400} />
-
-      <h3>Teslabot</h3>
-      <img src="/images/teslabot.jpg" alt="Teslabot" width={400} />
-    </div>
-  );
-}
 
